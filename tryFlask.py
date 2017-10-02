75d0f18ffb2c415322fed58eceba79a36adeb96b
serixen <s.petrovskuy@gmail.com>
2017-10-02 23:25:30 +0300

    addin signup page functional

--- tryFlask.py
+++ tryFlask.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from wtforms import DateField, validators, PasswordField, BooleanField, Form
 
 app = Flask(__name__)
 
@@ -8,13 +9,39 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/signup')
+@app.route('/signup/', methods=["GET", "POST"])
 def signup():
-    return render_template('signup.html')
+    try:
+        form = RegistartionForm(request.form)
 
-@app.route('/login')
+        if request.method == "POST" and form.validate():
+            userFirstName = form.userFirstName.data
+            userLastName = form.userLastName.data
+            userLogin = form.userLogin.data
+            userEmail = form.userEmail.data
+            userPassword = form.userPassword.data
+            return redirect(url_for('main'))
+
+        return render_template('signup.html', form=form)
+    except Exception as e:
+        return (str(e))
+
+
+@app.route('/login/')
 def login():
     return render_template('login.html')
+
+class RegistartionForm(Form):
+    userFirstName = DateField("First name", [validators.Length(min=4, max=20)])
+    userLastName = DateField("Last name", [validators.Length(min=4, max=20)])
+    userLogin = DateField("Login", [validators.Length(min=6, max=20)])
+    userEmail = DateField("Email", [validators.Length(min=6, max=50)])
+    userPassword = PasswordField("New password", [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message="Password must match")
+    ])
+    confirm = PasswordField('Repeat password')
+    accept_tos = BooleanField("I agree with rules.")
 
 if __name__ == '__main__':
     app.run(port=app.config.get("PORT", 9000))
