33f8982cd9f39dbdfe2e3802a6123ebb514fe525
serixen <s.petrovskuy@gmail.com>
2017-10-02 22:42:54 +0300

    Adding login and signup page

--- tryFlask.py
+++ tryFlask.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return render_template('main.html')
 
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(port=app.config.get("PORT", 9000))
